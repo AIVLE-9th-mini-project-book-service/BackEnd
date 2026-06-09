4cb54cc24930a2d80ca4fd70df49f43ee281de42
Bakhyoungwoo <toplionby421@gmail.com>
2026-06-09 16:53:47 +0900

    feat: 소프트 삭제 조회 포함 BookRepository 추가

---
+++ src/main/java/com/aivle/bookapp/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.aivle.bookapp.repository;
+
+import com.aivle.bookapp.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByDeletedAtIsNull();
+}
