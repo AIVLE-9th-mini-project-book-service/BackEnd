d2150f95c53d0f1e3969e2ebcdf89cb92f7987ba
garam <rkfka4291@naver.com>
2026-06-12 11:38:34 +0900

    관리자 스웨거 적용

--- src/main/java/com/aivle/bookapp/controller/AdminController.java
+++ src/main/java/com/aivle/bookapp/controller/AdminController.java
@@ -3,6 +3,9 @@ package com.aivle.bookapp.controller;
 import com.aivle.bookapp.dto.BookUpdateRequest;
 import com.aivle.bookapp.service.BookService;
 import com.aivle.bookapp.util.JwtUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@Tag(name = "Admin API", description = "관리자 관련 API")
 @RestController
 @RequestMapping("/admin")
 @RequiredArgsConstructor
@@ -17,12 +21,15 @@ public class AdminController {
     private final JwtUtil jwtUtil;
     private final BookService bookService;
 
+    @Schema(description = "관리자 이름")
     @Value("${admin.username}")
     private String adminUsername;
 
+    @Schema(description = "관리자 비밀번호")
     @Value("${admin.password}")
     private String adminPassword;
 
+    @Operation(summary = "관리자 로그인", description = "이름과 비밀번호를 입력하여 로그인합니다.")
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody Map<String, String> body) {
         String username = body.get("username");
@@ -36,6 +43,7 @@ public class AdminController {
         return ResponseEntity.ok(Map.of("accessToken", token));
     }
 
+    @Operation(summary = "관리자 도서 수정", description = "관리자 권한을 가진 사용자가 도서 정보를 수정합니다.")
     @PatchMapping("/books/{id}")
     public ResponseEntity<?> updateBook(
             @PathVariable Long id,
@@ -50,6 +58,7 @@ public class AdminController {
         return ResponseEntity.ok(bookService.adminUpdate(id, request));
     }
 
+    @Operation(summary = "관리자 도서 삭제", description = "관리자 권한을 가진 사용자가 도서를 삭제합니다.")
     @DeleteMapping("/books/{id}")
     public ResponseEntity<?> deleteBook(
             @PathVariable Long id,
